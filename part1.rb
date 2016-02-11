e57014c5a4f86657fe5588ce02c9d403eec46c37
William Durden <williamdurden@Williams-MacBook-Air.local>
2016-02-11 09:00:05 -0600

    need help on methods/syntax

--- part1.rb
+++ part1.rb
@@ -1,2 +1,12 @@
-takos="Tacos must not have lettuce Only meat and cheese and salsa"
- puts takos.split(" ")
+takos = ["Tacos must not have lettuce Only meat and cheese and salsa"]
+
+# turn takos and turn in to a string
+
+five =
+  takos.join(",")
+
+count =
+ fives.each do |five|
+   fives.char = (5)
+#take string and use select/find and do methods to--
+#--find word with char count of five
